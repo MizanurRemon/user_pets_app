0695b3b33954cb667b595acb5b5e3494d25c3c7a
Muhammad Umer <umershaikh217@gmail.com>
2022-01-30 13:40:33 +0500

    Recyler , (layout_change)

---
+++ app/src/main/java/com/example/databaseapp/MainActivity.java
@@ -0,0 +1,233 @@
+package com.example.databaseapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ContentUris;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+
+import android.net.Uri;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import android.widget.TextView;
+
+import com.example.databaseapp.ui.petAdapter;
+import com.example.databaseapp.ui.petContract.petEntry;
+import com.example.databaseapp.ui.petContract;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+
+
+    private static final int PET_LOADER = 0;
+    PetDbHelper mDbHelper ;
+   // PetCursorAdapter mCursorAdapter;
+   private RecyclerView mRecyclerView;
+    private petAdapter mRecyclerViewAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Change layout
+       // setContentView(R.layout.activity_main);
+
+         setContentView(R.layout.activity_main2);
+      //  ListView petListView = (ListView) findViewById(R.id.listView);
+         mDbHelper = new PetDbHelper(this);
+
+
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        //Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+       // mRecyclerView.setEmptyView(emptyView);
+
+       // mCursorAdapter = new PetCursorAdapter(this, null);
+
+      //  petListView.setAdapter( mCursorAdapter);
+
+       /* petListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this , EditorActivity.class);
+                Uri currentPetUri = ContentUris.withAppendedId(petEntry.CONTENT_URI , id);
+                intent.setData(currentPetUri);
+                startActivity(intent);
+
+
+            }
+        }); */
+
+        // Adding RecyclerView
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mRecyclerViewAdapter = new petAdapter(this);
+
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
+
+
+        LoaderManager.getInstance(this).initLoader(PET_LOADER, null, this);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_insert_dummy_data:
+                insertDb();
+                return true;
+
+
+            case R.id.action_delete_all_entries:
+                deleteAllPets();
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
+
+
+
+
+
+    private void insertDb(){
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(petEntry.COLUMN_PET_NAME , "Todo");
+        values.put(petEntry.COLUMN_PET_BREED , "Terrier");
+        values.put(petEntry.COLUMN_PET_GENDER , petEntry.GENDER_MALE);
+        values.put(petEntry.COLUMN_PET_WEIGHT , 7);
+
+
+        Log.i("MainActivity", "Dummy Data has been stored successfully ");
+
+        Uri newUri = getContentResolver().insert(petEntry.CONTENT_URI, values);
+
+    }
+
+
+
+
+
+
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+
+        String[] projection = {
+                petEntry._ID,
+                petEntry.COLUMN_PET_NAME,
+                petEntry.COLUMN_PET_BREED,
+                petEntry.COLUMN_PET_GENDER,
+                petEntry.COLUMN_PET_WEIGHT };
+
+
+
+        return new CursorLoader(this ,
+                petEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
+
+    }
+
+  /*  @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+         mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+    }  */
+
+
+
+        @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+            mRecyclerViewAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mRecyclerViewAdapter.swapCursor(null);
+    }
+
+
+
+
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(petEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+    }
+
+}
